d86992e74eb4160b9c7d2317bcbbdc1f957309e9
Zxander <Zxander@ZXANDERPC>
2023-01-22 17:52:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum is " + Summator.Sum(new int[] { 1, 2, 3 }));
+            Console.WriteLine("the average is " + Summator.Average(new int[] { 4, 5, 7 }));
+        }
+    }
+}
